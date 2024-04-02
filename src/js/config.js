57844a39ef7ec4356309700a38a9bbb86ad1ec5a
Sruthi-Eerati <sruthiyadav2002@gmail.com>
2024-04-02 13:41:40 +0530

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '68062454-6a39-4a03-91b6-4de12ac1b5c7';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/controller.js
@@ -0,0 +1,137 @@
+import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultView from './views/resultView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
+
+// https://forkify-api.herokuapp.com/v2
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //0.update results view to mark selected search results
+    resultView.update(model.getSearchResultsPage());
+
+    //1.updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    //2.loding recipe
+    await model.loadRecipe(id);
+
+    //3.rendering recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    recipeView.renderError();
+  }
+
+
+};
+
+const controlSearchResults = async function () {
+  try {
+
+    resultView.renderSpinner();
+
+    //1.get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2.load search results
+    await model.loadSearchResults(query);
+
+    //3.render results
+    resultView.render(model.getSearchResultsPage());
+
+    //4. render intial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  //1.render new results
+  resultView.render(model.getSearchResultsPage(goToPage));
+
+  //4. render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  //update the recipe view
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  //1. add and remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  //2.update recipe view
+  recipeView.update(model.state.recipe);
+
+  //3.render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe =  async function (newRecipe) {
+  try {
+    //show loading spinner
+    addRecipeView.renderSpinner();
+
+    //upload new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    //render recipe
+    recipeView.render(model.state.recipe);
+
+    //success message
+    addRecipeView.renderMessage();
+
+    //render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+
+    //change id in url
+    window.history.pushState(null, '',`#${model.state.recipe.id}`);
+  
+    //close form window
+    setTimeout(function() {
+      addRecipeView.toggleWindow()
+    }, MODAL_CLOSE_SEC * 1000);
+
+  } catch (err) {
+    console.error(err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+}
+init();
